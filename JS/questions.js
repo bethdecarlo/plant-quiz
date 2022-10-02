9bb81e31a603d001c1fde73652c78b455ae28ba4
Elizabeth DeCarlo <110702442+bethdecarlo@users.noreply.github.com>
2022-10-02 18:24:42 -0400

    added README.md, HMTL, CSS, and Javascript.

---
+++ JS/questions.js
@@ -0,0 +1,86 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Which plant is carnivorous?",
+    choices: ["All the below", "Pitcher Plant", "Sundew", "Venus Fly Trap"],
+    answer: "All the below"
+  },
+  {
+    title: "Which part of the flower is released into the air?",
+    choices: [
+      "Petals",
+      "Pollen",
+      "Roots",
+      "Stems"
+    ],
+    answer: "Pollen"
+  },
+  {
+    title: "Why do some plants have sharp thorns or spines?",
+    choices: ["to attract polinators", "to collect more water and sunlight", "as defence against animals that feed on them", "all of the above"],
+    answer: "as defence against animals that feed on them"
+  },
+  {
+    title: "Which is not actually a type of plant?",
+    choices: [
+      "Moss",
+      "Giant Water Lily",
+      "Baobob Tree",
+      "Mushroom"
+    ],
+    answer: "Mushroom"
+  },
+  {
+    title:
+      "The sap from the rubber tree is called",
+    choices: [
+      "Latex",
+      "Spandex",
+      "Flexmax",
+      "Index"
+    ],
+    answer: "Latex"
+  },
+  {
+    title: "Another common name for a snake plant is",
+    choices: [
+      "Mother-in-laws tongue",
+      "Frog's Legs",
+      "Beetlejuice",
+      "Toelio"
+    ],
+    answer: "Mother-in-laws tongue"
+  },
+  {
+    title: "This plant can be toxic if eaten or rubbed on the skin.",
+    choices: ["lavender", "dandelion", "pencil cactus", "sunflower"],
+    answer: "pencil cactus"
+  },
+  {
+    title: "Having house plants can",
+    choices: [
+      "remove air pollution and allergens",
+      "reduce your stress level",
+      "help reduce loud souds",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "The ancient art of growing miniture ornamental trees is called",
+    choices: ["tree origami", "meditation", "bonsai", "crochet"],
+    answer: "bonsai"
+  },
+  {
+    title:
+      "Which plant is the main food of pandas?",
+    choices: ["milkweed", "bananas", "palm", "bamboo"],
+    answer: "bamboo"
+  },
+  {
+    title: "A plant called Papyrus was used to create the cery first type of",
+    choices: ["Tortilla", "Boat", "Wooden leg", "Paper"],
+    answer: "Paper"
+  }
+];
